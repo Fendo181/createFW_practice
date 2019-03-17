46ee4b29f83b2a30e87d487f9331750ae359979b
Fendo181 <fendo181git@gmail.com>
2019-03-17 13:20:32 +0900

    フォーム画面にエラーを表示する処理を追加する

--- chap6/bbs.php
+++ chap6/bbs.php
@@ -7,6 +7,15 @@
 <body>
     <h1>遠藤:一言掲示板</h1>
     <form action="bbs_db.php" method="post">
+        <?php if(count($erros)): ?>
+        <ul class="erro-list">
+            <?php foreach ($errors as $error): ?>
+            <li>
+                <?php echo htmlspecialchars($error,ENT_QUOTES,'utf-8') ?>
+            </li>
+            <?php endforeach; ?>
+        </ul>
+        <?php endif ?>
         <P>
             名前:<input type="text" name="name">
             ひとこと:<input type="text" name="comment">
