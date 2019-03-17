aa60b53b52b9aa33eea9cb51ce9c5778607b5174
Fendo181 <fendo181git@gmail.com>
2019-03-17 12:54:35 +0900

    一言掲示板用のフォームページを作成した

---
+++ chap6/bbs.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title>
+        一言掲示板
+    </title>
+</head>
+<body>
+    <h1>遠藤:一言掲示板</h1>
+    <form action="bbs_db.php" method="post">
+        <P>
+            名前:<input type="text" name="name">
+            ひとこと:<input type="text" name="comment">
+            <input type="submit" name="submit" value="送信する">
+        </P>
+    </form>
+</body>
+</html>
