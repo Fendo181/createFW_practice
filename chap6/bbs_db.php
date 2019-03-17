fde52e8fa7726f3cc5dd7e346bed91f971be97b3
Fendo181 <fendo181git@gmail.com>
2019-03-17 13:20:13 +0900

    POSTで受け取った値を取得してDBに保存する

--- chap6/bbs_db.php
+++ chap6/bbs_db.php
@@ -19,3 +19,40 @@ try {
 } catch (PDOException $e) {
     echo 'Connection failed: ' . $e->getMessage();
 }
+
+$erros = [];
+
+// POSTなら保存処理を行う
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // 名前が正しく入力されているかチェック
+    $name = null;
+
+    if (!isset($_POST['name']) || !strlen($_POST['name'])) {
+        $erros['name'] = '名前を入力して下さい';
+    } else if (strlen($_POST['name']) > 40) {
+        $erros['name'] = '名前を40文字以内で入力して下さい';
+    } else {
+        $name = $_POST['name'];
+    }
+
+    // 一言が正しく入力されているかチェックする
+    $comment = null;
+
+    if (!isset($_POST['comment']) || !strlen($_POST['comment'])) {
+        $erros['comment'] = 'コメントを';
+    } else if (strlen($_POST['name']) > 200) {
+        $erros['comment'] = 'ひとことは200文字以内で入力して下さい';
+    } else {
+        $name = $_POST['name'];
+    }
+
+    //エラーがなければ保存する
+    if (count($erros) > 0) {
+        $sql = "INSERT INTO bbs_online (
+            `name`, `comment`, `created_at` 
+        ) VALUES (
+            $name, '$comment', 'date('Y-m-d H:i:s')'
+        )";
+        $res = $pdo->query($sql);
+    }
+}
