9a2e260bc30b85fbce7f06e39b905acdf5ae27f2
Fendo181 <fendo181git@gmail.com>
2019-03-25 00:13:38 +0900

    DBの接続情報を管理するconnectメソッドを追記した

--- chap7/core/DbManager.php
+++ chap7/core/DbManager.php
@@ -2,5 +2,34 @@
 
 class DbManager
 {
+    protected $connections = [];
+
+    /**
+     *
+     * PDOオブジェクトを使ってDBへの接続情報を管理する
+     *
+     * @param $name
+     * @param $params
+     */
+    public function connect($name,$params)
+    {
+        $params = array_merge([
+           'dsn' => null,
+           'user' => '',
+           'password' => '',
+           'options' => [],
+        ],$params);
+
+        $con = new PDO(
+            $params['dsn'],
+            $params['user'],
+            $params['password'],
+            $params['options']
+        );
+
+        $con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+        $this->connections[$name] = $con;
+    }
 
 }
