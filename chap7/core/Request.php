517f907d2a2444f354ce05dfe7461e057023e731
Fendo181 <fendo181git@gmail.com>
2019-03-24 19:56:49 +0900

    getPathInfoメソッドを追加した

--- chap7/core/Request.php
+++ chap7/core/Request.php
@@ -81,5 +81,46 @@ class Request
         return $_SERVER['REQUEST_URI'];
     }
 
+    /**
+     *
+     * スクリプトが実行されるファイルパスを返します。
+     *
+     * @return  スクリプトが実行されるbaseURLを返します
+     */
+    public function getBaseURL()
+    {
+        $scripts_name = $_SERVER['SCRIPT_NAME'];
+        $request_uri = $this->getRequestUri();
+
+        if(0 === strpos($request_uri, $scripts_name)){
+            return $scripts_name;
+        }else if (strpos($request_uri, dirname($scripts_name))){
+            return rtrim( dirname($scripts_name),'/');
+        }
+
+    }
+
+    /**
+     *
+     * baseURL以下のPATHの情報を取得する
+     *
+     * @return $path_info
+     */
+    public function getPathInfo()
+    {
+        $base_uri = $this->getBaseURL();
+        $request_uri = $this->getRequestUri();
+
+        // クエリーパラメータの場合
+        $pos = strpos($request_uri,'?');
+
+        if($pos !== false){
+            $request_uri = substr($request_uri,0, $pos);
+        }
+
+        $path_info = (string)substr($request_uri,strlen($base_uri));
+        return $path_info;
+    }
+
 }
 
