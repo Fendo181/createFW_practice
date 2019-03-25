f185b7e5ec6990fb132363d83f540e7ea9dc43ad
Fendo181 <fendo181git@gmail.com>
2019-03-25 11:18:18 +0900

    p236 コントローラーの呼び出しと実行の機能を追加した

--- chap7/core/Application.php
+++ chap7/core/Application.php
@@ -6,7 +6,7 @@
  * アプリケーションの中心となるクラスを
  *
  */
-class Application
+abstract class Application
 {
     protected $debug = false;
     protected $request;
@@ -166,5 +166,76 @@ class Application
     public function getWebDir()
     {
         return$this->getRootDir(). '/web';
+    }
+
+
+    /**
+     *
+     * Routerからコントローラーを特定して、runAction()を呼び出して実行して、レスポンスの送信を行う
+     *
+     */
+    public function run()
+    {
+        $params = $this->router->resolve($this->request->getPathInfo());
+        if($params === false){
+            // todo-A
+        }
+
+        $controller = $params['controller'];
+        $action = $params['action'];
+
+        $this->runAction($controller,$action,$params);
+        $this->response->send();
+    }
+
+    /**
+     *
+     * Controllerのアクションを実行するメソッド
+     *
+     * @param $controller_name
+     * @param $action
+     * @param array $params
+     */
+    public function runAction($controller_name,$action,$params = [])
+    {
+        // ルーティングはコントローラーの小文字を指定するようにしたので、ucfirstで大文字に変換する
+        $controller_class = ucfirst($controller_name).'Controller';
+
+        $controller = $this->findController($controller_class);
+        if($controller === false){
+            // todo-B
+        }
+
+        $content = $controller->run($action,$params);
+
+        // コンテンツをセットする
+        $this->response->setContent($content);
+    }
+
+    /**
+     *
+     * コントローラークラスが読み込まれてない場合にクラスファイルを読み込みます。
+     *
+     * @param $controller_class
+     * @return Controllerインスタンス | false
+     */
+    public function findController($controller_class)
+    {
+        if(!class_exists($controller_class)){
+            $controller_file = $this->getControllerDir(). '/' . $controller_class.'php';
+        }
+
+        if(!is_readable($controller_file)){
+            return false;
+        }else{
+
+            require_once $controller_file;
+
+            if(!class_exists($controller_file)){
+                return false;
+            }
+        }
+
+        return new $controller_file;
     }
 }
