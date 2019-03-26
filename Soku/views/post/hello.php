688975c7062e6422a18fc79cec640adb2ab4bc5f
endu <futoshi.endo@pepabo.com>
2019-03-26 21:23:58 +0900

    hello時にviewファイルを作成した

---
+++ Soku/views/post/hello.php
@@ -0,0 +1,16 @@
+<?php
+
+$this->setLayoutVar('title','Hello画面');
+
+?>
+
+<html>
+<head>
+    <title>
+        Hello! Soku!
+    </title>
+</head>
+<body>
+    <h1>Hello Wold!</h1>
+</body>
+</html>
