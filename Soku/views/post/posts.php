46302af49c0ac349095cfedd2fdd5cac6b7a97da
Fendo181 <fendo181git@gmail.com>
2019-03-27 10:12:01 +0900

    投稿先一覧を表示するpostViewファイルを作成した

--- Soku/views/post/posts.php
+++ Soku/views/post/posts.php
@@ -5,3 +5,11 @@ $this->setLayoutVar('title','Hello画面');
 ?>
 
 <h1>投稿先一覧です</h1>
+<div class="posts">
+    <?php foreach ($posts as $post ): ?>
+        <ul>
+            <li>名前:<?= $this->escape($post['name']); ?>(投稿時間:<?= $this->escape($post['created_at']); ?>) </li>
+            <li>コメント:<?= $this->escape($post['comment']); ?></li>
+        </ul>
+    <?php endforeach; ?>
+</div>
