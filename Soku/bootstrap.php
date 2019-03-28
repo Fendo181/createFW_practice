eb7ef912d0c539f9443d8da7d337a72a04d30c14
Fendo181 <fendo181git@gmail.com>
2019-03-28 10:59:01 +0900

    Illuminate Containerのインスタンスを宣言する

--- Soku/bootstrap.php
+++ Soku/bootstrap.php
@@ -1,6 +1,9 @@
 <?php
 
+require dirname(__DIR__)."/vendor/autoload.php";
 require 'core/ClassLoader.php';
+
+$app = new \Illuminate\Container\Container;
 
 $loader = new ClassLoader();
 $loader->registerDir(dirname(__FILE__).'/core');
